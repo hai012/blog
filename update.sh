760f0197ab4d1a39a6d0174c1bbe6595d1aac496
ghjiee <9637301+ghjiee@user.noreply.gitee.com>
2022-04-07 10:36:37 +0800

    update

--- update.sh
+++ update.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
+
+
 git config --global user.name "ghjiee"
 git config --global user.email "9637301+ghjiee@user.noreply.gitee.com"
-cp ~/shared_198/doc/*  .  -r
-git add -A
-git commit -m "update"
-git push origin master
+
+
+if [ "$1" == "remote" ]
+then
+	rm -rf ~/doc/blog/*
+	cp ~/shared_198/doc/*  .  -r
+	git add -A
+	git commit -m "update"
+	git push origin master
+elif [ "$1" == "local" ]
+then
+	git pull
+	rm -rf ~/shared_198/doc/*
+	cp ~/doc/blog/*  ~/shared_198/doc/  -r
+else
+	echo "usage: ./update <local/remote>"
+fi
