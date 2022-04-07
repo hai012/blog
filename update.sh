288afd00533e3765bc4e0928284c7dd693e76c85
ghjiee <9637301+ghjiee@user.noreply.gitee.com>
2022-04-07 10:12:11 +0800

    update

--- update.sh
+++ update.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+git config --global user.name "ghjiee"
+git config --global user.email "9637301+ghjiee@user.noreply.gitee.com"
 cp ~/shared_198/doc/markdown . -r
 git add -A
 git commit -m "update"
